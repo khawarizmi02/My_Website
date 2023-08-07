53765ed2729d8f958b1760ac16bbe4108edca337
khawarizmi02 <khawarizmijefri02@gmail.com>
2023-08-07 09:50:59 +0800

    done designing nav bar fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
+// import { Switch, Route, Link, Routes } from 'react-router-dom';
+// import { Home, About } from '../src/pages'
+
 import styles from './style';
 import './index.css';
 import { Hero, Bullet, Navbar } from './components';
 
 function App() {
   return (
-    <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Navbar />
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter} bg-black`}>
+          <div className={`${styles.boxWidth} bg-black`}>
+            <Navbar />
+          </div>
         </div>
+        
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+      
+            <Bullet />
       </div>
-      <Hero />
-      <Bullet />
-    </div>
   );
 }
 
---
+++ src/constant/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "location",
+    title: "Location",
+  },
+  {
+    id: "contact-us",
+    title: "Contact Us",
+  },
+];
