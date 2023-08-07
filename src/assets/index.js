50f9089ef47ad81a2156aac29953d3582ce1926c
khawarizmi02 <khawarizmijefri02@gmail.com>
2023-08-07 17:18:23 +0800

    add images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,17 @@
 import logo from "./logo.svg"
 import menu from "./menu.svg"
 import close from "./close.svg"
+import people from "./people.svg"
+import service from "./service.svg"
+import schedule from "./schedule.svg"
+import heroImage from "./heroImage.svg"
 
 export {
   logo,
   menu,
   close,
+  people,
+  service,
+  schedule,
+  heroImage,
 }
