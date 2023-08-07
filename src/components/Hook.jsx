0a3c0aa9b6272ad8fb3d9e819d651b99be8953b3
khawarizmi02 <khawarizmijefri02@gmail.com>
2023-08-07 17:19:27 +0800

    add hook compoenent

---
+++ src/components/Hook.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from '../style'
+
+const Hook = () => {
+  return (
+    <div className={styles.hook}>
+      Hello this is hook
+    </div>
+  )
+}
+
+export default Hook
