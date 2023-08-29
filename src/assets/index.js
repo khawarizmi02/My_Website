ffbcbaffe3f30f8586602887ebccdd36508a9a55
khawarizmi02 <khawarizmijefri02@gmail.com>
2023-08-29 20:53:19 +0800

    add more icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,7 @@ import twitter from "./twitter.svg"
 import instagram from "./instagram.svg"
 import facebook from "./facebook.svg"
 import linkedin from "./linkedin.svg"
+import whatsappIcon from "./whatsappicon.svg"
 import whatsapp1 from "./whatsapp1.png"
 import lowlogo from "./lowlogo.svg"
 import logo1 from "./logo1.svg"
@@ -44,7 +45,7 @@ export {
   menu,
   close,
   heroImage,
-  twitter, instagram, facebook, linkedin, whatsapp1,
+  twitter, instagram, facebook, linkedin, whatsapp1, whatsappIcon,
   lowlogo,
   logo1,
   photo1, photo3, photo5, photo8,photo9,photo11,photo16,photo17,photo18,photo19,
