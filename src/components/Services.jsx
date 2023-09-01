7d06406933eb635f81b4bf010667372882a218b6
khawarizmi02 <khawarizmijefri02@gmail.com>
2023-09-02 01:44:16 +0800

    update service component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from '../style'
 import { ServicesContent as content } from '../constant'
@@ -14,7 +15,7 @@ const Services = () => {
           We deal with all kinds of pest that interfere with your peace a comfort of livings.
         </p>
         <p className={`${styles.paragraph4} max-w-[967px]`}>
-          Hear are the list of services the we can provide:
+          Here are the list of services the we can provide:
         </p>
       </div>
       <div className="flex flex-wrap justify-center sm:justify-start items-center w-full feedback-container relative z-[1]">
@@ -24,13 +25,16 @@ const Services = () => {
           
             <div className="relative w-[147px] h-[147px] hover:scale-105 transition-transform duration-200">
               <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-                              feedback-card-brown w-[170px] h-[170px] z-[-1] "></div>
-              <img src={item.image} alt={item.id} className="w-full h-full rounded-full z-10" />
+                              feedback-card-brown w-[170px] h-[170px] z-[-1] ">
+              </div>
+              <Link to={`/services/${item.id}-services`}><img src={item.image} alt={item.id} className="w-full h-full rounded-full z-10" /></Link>
             </div> 
             
-            <div className='flex items-center'>
-              <div className={`${styles.point} mt-5 capitalize text-center max-w-full`}> {item.name} </div>
-            </div>
+            <Link to={`/services/${item.id}`}>
+              <div className='flex items-center'>
+                <div className={`${styles.point} mt-5 capitalize text-center max-w-full`}> {item.name} </div>
+              </div>
+            </Link>
 
           </div>
         ))}
