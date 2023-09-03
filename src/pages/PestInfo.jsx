2fdfc5c85b04a89edf829e4380e11f62583148c7
khawarizmi02 <khawarizmijefri02@gmail.com>
2023-09-04 04:12:43 +0800

    done pests library

--- src/pages/PestInfo.jsx
+++ src/pages/PestInfo.jsx
@@ -1,8 +1,94 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import styles from '../style'
 import { Navbar, Footer } from '../components'
+import { urlFor, client } from '../client'
+
+const Info = ({ pestId }) => {
+  
+  const [pests, setPests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    
+    const query = '*[_type == "library"]';
+    setIsLoading(true);
+
+    client.fetch(query).then((data) => {
+      setPests(data);
+      setIsLoading(false);
+    })
+  }, [])
+
+  if (isLoading) {
+    return <div 
+    className='flex flex-col justify-center center 
+    font-poppins font-[20px] text-black text-center'
+    >
+    Loading...
+    </div>;  // Render a loading message while data is being fetched
+  }
+  
+  const filteredPest = pests.find( item => item.slug.current === pestId) 
+  const listedPests = pests.filter( item => item.slug.current !== pestId)
+
+  return (
+    <>
+      <section className={`${styles.flexStart} pb-3 relative`}>
+        <div className='flex flex-col items-center justify-center'>
+          <img src={urlFor(filteredPest.pestImage).url()} className='w-full max-h-[450px] object-cover' />
+          <div className='font-poppins font-bold text-black text-center text-[45px] capitalize'>
+            {filteredPest.pestName}
+          </div>
+        </div>
+      </section>
+      <section className={`${styles.paddingY} px-3`}>
+        <div className='grid grid-cols-[80%,20%]'>
+          <div className='py-3 px-3'> 
+            <div className='py-5 px-3'>
+              <div className='font-poppins font-bold text-black text-[35px] border-b-2 border-blackBlur pb-2'> 
+                Introduction 
+              </div>
+              <div className={`${styles.paragraph2}`}> {filteredPest.info}</div>
+            </div>
+            <div className='py-5 px-3'>
+              <div className='font-poppins font-bold text-black text-[35px] border-b-2 border-blackBlur pb-2'>
+                Lifecycle 
+              </div>
+              <div className={`${styles.paragraph2}`}> {filteredPest.lifecycle}</div>
+            </div>
+            <div className='py-5 px-3'>
+              <div className='font-poppins font-bold text-black text-[35px] border-b-2 border-blackBlur pb-2'>
+                Importance 
+              </div>
+              <div className={`${styles.paragraph2}`}> {filteredPest.importance} </div>
+            </div>
+          </div>
+          <div className='flex flex-col p-3'>
+            <div 
+              className='font-poppins font-medium text-black text-[20px] text-center
+                         py-3 px-3'> 
+              Read more about other pest: 
+            </div>
+            <div> 
+              {listedPests.map((item) => (
+                <div className='py-5 min-w-[120px]'>
+                  <div className='max-w-full min-w-[120px] max-h-[350px] sm:h-[145px] overflow-hidden rounded-[10px] relative pt-3'>
+                    <img src={urlFor(item.pestImage).url()} className='absolute top-0 left-0 w-full h-full object-cover'/>
+                  </div>
+                  <div className='pt-3 font-poppins font-medium capitalize text-[20px] text-black'> {item.pestName} </div>
+                  <Link to={`/pests/${item.slug.current}`} className='font-poppins font-bold text-[15px] text-black'>Read More +</Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
 
 const PestInfo = () => {
   
@@ -17,8 +103,7 @@ const PestInfo = () => {
       </div>
       <div className={`bg-cream ${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          {pestId} page
-
+          <Info pestId={pestId}/>
           <Footer />
         </div>
       </div>
--- src/pages/Pests.jsx
+++ src/pages/Pests.jsx
@@ -1,7 +1,83 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from '../style'
 import { Navbar, Footer } from '../components'
+import { urlFor, client } from '../client'
+import { photo20 } from '../assets'
+
+const PestList = () => {
+
+  const [pests, setPests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    
+    const query = '*[_type == "library"]';
+    setIsLoading(true);
+
+    client.fetch(query).then((data) => {
+      setPests(data);
+      setIsLoading(false);
+    })
+  }, [])
+
+  if (isLoading) {
+    return <div 
+    className='flex flex-col justify-center center 
+    font-poppins font-[20px] text-black text-center'
+    >
+    Loading...
+    </div>;  // Render a loading message while data is being fetched
+  }
+
+  return (
+    <section className={`${styles.flexStart} ${styles.paddingX} ${styles.paddingY}`}>
+      <div className='flex flex-col justify-center items-center'>
+        <div className={`${styles.heading1} text-center`}> Pest Library </div>
+        <div className='font-poppins font-light text-[20px]'> Dive deep into the world of pests and discover what's lurking in the shadows. </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-6'>
+          {pests.map((item) => (
+            <div className='flex flex-col w-full'>
+              <div>
+                <img src={urlFor(item.pestImage).url()} className='px-none sm:px-3 py-3 w-full h-[300px] object-cover rounded-[30px]' />
+              </div>
+              <div className='font-poppins font-bold text-black text-center text-[20px]'>
+                <Link to={`/pests/${item.slug.current}`}>{item.pestName}</Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const Intro = () => {
+  return (
+    <section className={`${styles.paddingX} pb-3`}>
+      <div className='grid grid-cols-1 md:grid-cols-[40%,60%] gap-6'>
+        <div className='text-center px-6 py-3 w-full'> 
+          <img src={photo20} className='w-full h-full object-cover rounded-[10px]'/>
+        </div>
+        <div className='flex flex-col w-full justify-center'> 
+          <div className={`${styles.point} capitalize`}>
+            Specialised IPM Solutions
+          </div>
+          <div className={`${styles.paragraph2} pt-3`}>
+            Evolution over millions of years has made pests adaptable to challenging circumstances. 
+            Some pests are able to survive and multiply in threatening environments and can even survive indiscriminate chemical spraying.
+            This is where comprehensive solutions for pest management in Malaysia come in.
+          </div>
+          <div className={`${styles.paragraph2} pt-3`}>
+            Our Integrated Pest Management plan begins with preliminary inspections of the affected area to identify the pest species and conceptualising plans to exploit their weaknesses, in addition to establishing the food sources used to sustain the infestation. After surveying the pest problem, we formulate a strategy for customised IPM pest control services and strike with full force to disorientate the pests and ensure they are unable to propagate. Follow-up inspections ensure that all remaining stragglers are eliminated.
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 const Pests = () => {
   return (
@@ -13,8 +89,8 @@ const Pests = () => {
       </div>
       <div className={`bg-cream ${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Pests Library page
-
+          <Intro />
+          <PestList />
           <Footer />
         </div>
       </div>
